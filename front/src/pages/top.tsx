6ab4e0739b9f7a7295d173978e0667ae11c2fae4
ニシ <bunsekimal@gmail.com>
2025-03-24 23:10:03 +0900

    一覧表示

--- front/src/pages/top.tsx
+++ front/src/pages/top.tsx
@@ -1,3 +1,15 @@
+import { useContext } from "react";
+import { TaskContext } from "@/providers/task";
+
+
 export function Top() {
-  return <div>Top Page</div>;
+  const { tasks } = useContext(TaskContext);
+
+  return (
+    <ul>
+      {tasks.map(task => (
+        <li key={task.id}>{task.title}</li>
+      ))}
+    </ul>
+  );
 }
